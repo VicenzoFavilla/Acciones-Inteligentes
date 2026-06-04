d21a8b23aaaa5369d87384c625185dac33e78a56
VicenzoFavilla <favillavicenzo@gmail.com>
2026-06-04 06:30:28 -0300

    Fix de errores

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -2,12 +2,8 @@ from pydantic import BaseSettings, Field
 
 class Settings(BaseSettings):
     # MongoDB Configuration
-<<<<<<< HEAD
     # CAMBIA ESTA LÍNEA: de 'mongodb' a 'localhost' o '127.0.0.1'
     MONGO_URL: str = Field(default="mongodb://localhost:27017/acciones_ml", description="MongoDB Connection URL")
-=======
-    MONGO_URL: str = Field(default="mongodb://127.0.0.1:27017/acciones_ml", description="MongoDB Connection URL")
->>>>>>> 6354d4c99e59e5dd8f81ffcc13fc2e7fd36e8db7
     MONGO_INITDB_ROOT_USERNAME: str = Field(default="root", description="MongoDB Root Username")
     # ... el resto de tu código sigue igual
     MONGO_INITDB_ROOT_PASSWORD: str = Field(default="example", description="MongoDB Root Password")
